74adc60a4a0534fc80856ce4a5b3cab8c460e09d
vvikobra <viktoria.obrokova@yandex.ru>
2024-12-16 11:17:57 +0300

    add security configuration

---
+++ src/main/java/com/example/potteryworkshop/config/ApplicationSecurityConfiguration.java
@@ -0,0 +1,83 @@
+package com.example.potteryworkshop.config;
+
+import com.example.potteryworkshop.models.enums.UserRoles;
+import com.example.potteryworkshop.repositories.UserRepository;
+import com.example.potteryworkshop.services.Impl.UserDetailsServiceImpl;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.DelegatingSecurityContextRepository;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
+
+@Configuration
+public class ApplicationSecurityConfiguration {
+    private UserRepository userRepository;
+
+    public ApplicationSecurityConfiguration(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, SecurityContextRepository securityContextRepository) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        authorizeHttpRequests ->
+                                authorizeHttpRequests.
+                                        requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                                        .permitAll()
+                                        .requestMatchers("/favicon.ico").permitAll()
+                                        .requestMatchers("/error").permitAll()
+                                        .requestMatchers("/", "/users/login", "/users/register", "/users/login-error")
+                                        .permitAll().
+                                        requestMatchers("/users/profile").authenticated().
+                                        requestMatchers("/employees/add", "/employees/employee-delete/").hasRole(UserRoles.ADMIN.name()).
+                                        requestMatchers("/companies/company-delete/").hasRole(UserRoles.ADMIN.name()).
+                                        anyRequest().authenticated()
+                )
+                .formLogin(
+                        (formLogin) ->
+                                formLogin.
+                                        loginPage("/users/login").
+                                        usernameParameter("email").
+                                        passwordParameter("password").
+//                                        usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY).
+//                                        passwordParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY).
+                                        defaultSuccessUrl("/").
+                                        failureForwardUrl("/users/login-error")
+                )
+                .logout((logout) ->
+                        logout.logoutUrl("/users/logout").
+                                logoutSuccessUrl("/").
+                                invalidateHttpSession(true)
+                ).securityContext(
+                        securityContext -> securityContext.
+                                securityContextRepository(securityContextRepository)
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public SecurityContextRepository securityContextRepository() {
+        return new DelegatingSecurityContextRepository(
+                new RequestAttributeSecurityContextRepository(),
+                new HttpSessionSecurityContextRepository()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() { return new UserDetailsServiceImpl(userRepository); }
+}
